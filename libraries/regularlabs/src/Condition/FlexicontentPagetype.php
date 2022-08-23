a2545a14f56c485b655b88d5a1338b8244d358cd
Heena Chhabra <chh.odesk.mca@gmail.com>
2022-08-23 12:53:34 +0530

    upload code

---
+++ libraries/regularlabs/src/Condition/FlexicontentPagetype.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package         Regular Labs Library
+ * @version         22.8.11825
+ * 
+ * @author          Peter van Westen <info@regularlabs.com>
+ * @link            http://regularlabs.com
+ * @copyright       Copyright © 2022 Regular Labs All Rights Reserved
+ * @license         http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+namespace RegularLabs\Library\Condition;
+
+defined('_JEXEC') or die;
+
+/**
+ * Class FlexicontentPagetype
+ * @package RegularLabs\Library\Condition
+ */
+class FlexicontentPagetype extends Flexicontent
+{
+    public function pass()
+    {
+        return $this->passByPageType('com_flexicontent', $this->selection, $this->include_type);
+    }
+}
