ca8d920cfa0e9a422866db8f4608f74731847022
Heena Chhabra <chh.odesk.mca@gmail.com>
2022-08-23 20:33:54 +0530

    upload code

--- templates/shaper_gazette/overrides/com_content/category/blog.php
+++ templates/shaper_gazette/overrides/com_content/category/blog.php
@@ -4,9 +4,9 @@
  * @author JoomShaper https://www.joomshaper.com
  * @copyright Copyright (c) 2010 - 2021 JoomShaper
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
-*/
+ */
 
-defined ('_JEXEC') or die();
+defined('_JEXEC') or die();
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
@@ -18,36 +18,79 @@ HTMLHelper::addIncludePath(JPATH_COMPONENT . '/helpers');
 $app = Factory::getApplication();
 
 $this->category->text = $this->category->description;
-$app->triggerEvent('onContentPrepare', array($this->category->extension . '.categories', &$this->category, &$this->params, 0));
+$app->triggerEvent('onContentPrepare', [
+    $this->category->extension . '.categories',
+    &$this->category,
+    &$this->params,
+    0,
+]);
 $this->category->description = $this->category->text;
 
-$results = $app->triggerEvent('onContentAfterTitle', array($this->category->extension . '.categories', &$this->category, &$this->params, 0));
+$results = $app->triggerEvent('onContentAfterTitle', [
+    $this->category->extension . '.categories',
+    &$this->category,
+    &$this->params,
+    0,
+]);
 $afterDisplayTitle = trim(implode("\n", $results));
 
-$results = $app->triggerEvent('onContentBeforeDisplay', array($this->category->extension . '.categories', &$this->category, &$this->params, 0));
+$results = $app->triggerEvent('onContentBeforeDisplay', [
+    $this->category->extension . '.categories',
+    &$this->category,
+    &$this->params,
+    0,
+]);
 $beforeDisplayContent = trim(implode("\n", $results));
 
-$results = $app->triggerEvent('onContentAfterDisplay', array($this->category->extension . '.categories', &$this->category, &$this->params, 0));
+$results = $app->triggerEvent('onContentAfterDisplay', [
+    $this->category->extension . '.categories',
+    &$this->category,
+    &$this->params,
+    0,
+]);
 $afterDisplayContent = trim(implode("\n", $results));
 
 $plugin = JPluginHelper::getPlugin('content', 'jw_allvideos');
-                        
+
+
 // Check if plugin is enabled
-if ($plugin)
-{
+if ($plugin) {
     // Get plugin params
-$pluginParams = new JRegistry($plugin->params);
+    $pluginParams = new JRegistry($plugin->params);
 
-$param1 = $pluginParams->get('vfolder');
-
+    $param1 = $pluginParams->get('vfolder');
+    // echo'<pre>';
+    // print_r($param1);
 }
 ?>
-<div class="avPlayerWrapper avVideo">
-    <div class="avPlayerContainer text-center">
-        <div id="" class="avPlayerBlock"><video class="avPlayer" style="width:600px;height:450px;" src="<?php echo JURI::base().$param1; ?>/a-nation-in-decline.mp4" preload="metadata" controls="" controlslist=""></video></div>
-            <div class="avDownloadLink">
-                <a target="_blank" href="<?php echo JURI::base(); ?>/<?php echo $param1; ?>/a-nation-in-decline.mp4" download="">Download</a>
-            </div>
-        </div>
-    </div>
+<div class="blog<?php echo $this->pageclass_sfx; ?>">
+<?php
+if (is_dir($param1)) {
+    if ($dh = opendir($param1)) {
+        while (($file = readdir($dh)) !== false) {
+            // echo "filename: .".$file."<br />";
+            if (preg_match('/^.*\.(mp4|mov)$/i', $file)) {
+                //echo $file; ?>
+                <div class="article-list articles-leading<?php echo $this->params->get('blog_class_leading'); ?>">
+                <div class="avPlayerWrapper avVideo">
+                    <div class="avPlayerContainer text-center">
+                        <div id="" class="avPlayerBlock"><video class="avPlayer" style="width:600px;height:450px;" src="<?php echo JURI::base() .
+                            $param1; ?>/<?php echo $file?>" preload="metadata" controls="" controlslist=""></video>
+                        </div>
+                        <div class="avDownloadLink">
+                            <a target="_blank" href="<?php echo JURI::base(); ?>/<?php echo $param1; ?>/<?php echo $file?>" download="">Download</a>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                
+                <?php
+            }
+        }
+        closedir($dh);
+    }
+}
+?>
+
+    
 </div>
